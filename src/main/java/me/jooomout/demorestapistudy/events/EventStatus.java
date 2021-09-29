6c405802add95edae1eda1bc8ec11d2e3f06857c
jooomOut <rimeilo324@naver.com>
2021-09-29 23:00:52 +0900

    Feat: make event domain

---
+++ src/main/java/me/jooomout/demorestapistudy/events/EventStatus.java
@@ -0,0 +1,5 @@
+package me.jooomout.demorestapistudy.events;
+
+public enum EventStatus {
+    DRAFT, PUBLISHED, BEGIN_ENROLLMENT;
+}
---
+++ src/test/java/me/jooomout/demorestapistudy/events/EventTest.java
@@ -0,0 +1,32 @@
+package me.jooomout.demorestapistudy.events;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class EventTest {
+
+    @Test
+    void builder() {
+        Event event = Event.builder()
+                .name("Rest Study")
+                .description("REST API study - Spring")
+                .build();
+
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    void isJavaBean() {
+        Event event = new Event();
+        String name = "Event";
+        event.setName(name);
+        String description = "Spring";
+        event.setDescription(description);
+
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+}
