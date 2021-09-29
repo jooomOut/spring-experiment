aa1752c8e2897198837355c6c71e5c0787510bbf
jooomOut <rimeilo324@naver.com>
2021-09-29 23:28:06 +0900

    optimize import

--- src/test/java/me/jooomout/demorestapistudy/events/EventControllerTests.java
+++ src/test/java/me/jooomout/demorestapistudy/events/EventControllerTests.java
@@ -1,16 +1,12 @@
 package me.jooomout.demorestapistudy.events;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
--- src/test/java/me/jooomout/demorestapistudy/events/EventTest.java
+++ src/test/java/me/jooomout/demorestapistudy/events/EventTest.java
@@ -3,7 +3,6 @@ package me.jooomout.demorestapistudy.events;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 
 class EventTest {
