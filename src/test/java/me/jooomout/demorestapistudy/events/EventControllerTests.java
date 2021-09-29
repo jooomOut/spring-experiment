847b87e8226fdbda817b27d03178c508ee8de599
jooomOut <rimeilo324@naver.com>
2021-09-30 00:13:32 +0900

    Test: mocking event repository

--- src/test/java/me/jooomout/demorestapistudy/events/EventControllerTests.java
+++ src/test/java/me/jooomout/demorestapistudy/events/EventControllerTests.java
@@ -2,8 +2,10 @@ package me.jooomout.demorestapistudy.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,10 +26,12 @@ public class EventControllerTests {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    EventRepository eventRepository;
+
     @Test
     void createEvent() throws Exception {
         Event event = new Event().builder()
-                .id(12)
                 .name("Spring")
                 .description("REST API - SPRING")
                 .beginEnrollmentDateTime(LocalDateTime.of(2021, 9, 29, 23, 36))
@@ -39,6 +43,8 @@ public class EventControllerTests {
                 .limitOfEnrollment(100)
                 .location("중앙대")
                 .build();
+        event.setId(10);
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
 
         mockMvc.perform(post("/api/events/")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
