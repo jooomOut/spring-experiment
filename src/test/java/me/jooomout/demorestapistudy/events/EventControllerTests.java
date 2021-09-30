40f788b6db3cea3a19f9d862b9b634ceee5cd02c
jooomOut <rimeilo324@naver.com>
2021-09-30 22:24:40 +0900

    Test: MockBean 제거
    Repository 를 MockBean이 아닌 것으로 사용함

--- src/test/java/me/jooomout/demorestapistudy/events/EventControllerTests.java
+++ src/test/java/me/jooomout/demorestapistudy/events/EventControllerTests.java
@@ -1,10 +1,13 @@
 package me.jooomout.demorestapistudy.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
@@ -17,8 +20,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-// @RunWith(SpringRunner.class)
-@WebMvcTest
+
+//@WebMvcTest // for slicing test
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EventControllerTests {
 
     @Autowired
@@ -26,12 +31,10 @@ public class EventControllerTests {
     @Autowired
     ObjectMapper objectMapper;
 
-    @MockBean
-    EventRepository eventRepository;
-
     @Test
     void createEvent() throws Exception {
         Event event = new Event().builder()
+                .id(100)
                 .name("Spring")
                 .description("REST API - SPRING")
                 .beginEnrollmentDateTime(LocalDateTime.of(2021, 9, 29, 23, 36))
@@ -42,9 +45,10 @@ public class EventControllerTests {
                 .maxPrice(200)
                 .limitOfEnrollment(100)
                 .location("중앙대")
+                .free(true)
+                .offline(false)
                 .build();
-        event.setId(10);
-        Mockito.when(eventRepository.save(event)).thenReturn(event);
+
 
         mockMvc.perform(post("/api/events/")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
@@ -55,6 +59,10 @@ public class EventControllerTests {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("id").exists())
                 .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE));
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT))
+                ;
     }
 }
