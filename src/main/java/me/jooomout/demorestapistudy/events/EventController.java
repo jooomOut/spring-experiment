ea7312036fb7fa98dab4b1ad0dbe2fede9531805
jooomOut <rimeilo324@naver.com>
2021-09-30 22:24:08 +0900

    Feat: EventDto 추가
    ModelMapper -> Event Entity 를 Dto로

--- src/main/java/me/jooomout/demorestapistudy/events/EventController.java
+++ src/main/java/me/jooomout/demorestapistudy/events/EventController.java
@@ -1,8 +1,10 @@
 package me.jooomout.demorestapistudy.events;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +18,18 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 public class EventController {
 
     private final EventRepository eventRepository;
+    private final ModelMapper modelMapper;
 
-    public EventController(EventRepository eventRepository) {
+    public EventController(EventRepository eventRepository, ModelMapper modelMapper) {
         this.eventRepository = eventRepository;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody Event event) {
-        Event newEvent = eventRepository.save(event);
+    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+        Event event = modelMapper.map(eventDto, Event.class);
+
+        Event newEvent = this.eventRepository.save(event);
         URI createdUri = linkTo(EventController.class)
                 .slash(newEvent.getId()).toUri();
 
---
+++ src/main/java/me/jooomout/demorestapistudy/events/EventDto.java
@@ -0,0 +1,23 @@
+package me.jooomout.demorestapistudy.events;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class EventDto {
+
+    private String name;
+    private String description;
+
+    private LocalDateTime beginEnrollmentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime beginEventDateTime;
+    private LocalDateTime endEventDateTime;
+
+    private String location; // (optional)
+    private int basePrice; // optional
+    private int maxPrice; // optional
+    private int limitOfEnrollment;
+
+}
