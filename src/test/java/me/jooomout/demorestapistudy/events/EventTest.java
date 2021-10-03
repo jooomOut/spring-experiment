c6d721c5b6ec2658a7bb0fa5369194fc5f71ce93
jooomOut <rimeilo324@naver.com>
2021-10-03 22:41:27 +0900

    Test: ParameterizedTest

--- src/test/java/me/jooomout/demorestapistudy/events/EventTest.java
+++ src/test/java/me/jooomout/demorestapistudy/events/EventTest.java
@@ -1,6 +1,9 @@
 package me.jooomout.demorestapistudy.events;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,48 +32,46 @@ class EventTest {
         assertThat(event.getDescription()).isEqualTo(description);
     }
 
-    @Test
-    void testFree() {
+    @ParameterizedTest(name = "{index} =>  basePrice={0}, maxPrice={1}, isFree={2}")
+    @MethodSource("paramsForTestFree")
+    void testFree(int basePrice, int maxPrice, boolean isFree) {
         // GIVEN
         Event event = Event.builder()
-                .basePrice(0)
-                .maxPrice(0)
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
                 .build();
-
         // WHEN
         event.update();
-
         // THEN
-        assertThat(event.isFree()).isTrue();
-
-        //GIVEN
-        event = Event.builder()
-                .basePrice(0)
-                .maxPrice(100)
-                .build();
-
-        // WHEN
-        event.update();
-
-        // THEN
-        assertThat(event.isFree()).isFalse();
+        assertThat(event.isFree()).isEqualTo(isFree);
     }
 
-    @Test
-    void testOffline(){
+    private static Object[] paramsForTestFree() {
+        return new Object[] {
+                new Object[] {0,0,true},
+                new Object[] {100, 0, false},
+                new Object[] {100, 200, false}
+        };
+    }
+
+
+    @ParameterizedTest(name = "{index} =>  location={0}, isOffline={1}")
+    @MethodSource("paramsForTestOffline")
+    void testOffline(String location, boolean isOffline){
         Event event = Event.builder()
-                .location("우리집")
+                .location(location)
                 .build();
         event.update();
-        assertThat(event.isOffline()).isTrue();
+        assertThat(event.isOffline()).isEqualTo(isOffline);
 
-        event = Event.builder()
-                .build();
-        event.update();
-        assertThat(event.isOffline()).isFalse();
     }
-
-
+    private static Object[] paramsForTestOffline() {
+        return new Object[] {
+                new Object[] {"", false},
+                new Object[] {"우리집", true},
+                new Object[] {null, false}
+        };
+    }
 
 
 }
