9f628aed476783180279c5e467f2b1897ce880e9
jooomOut <rimeilo324@naver.com>
2021-10-08 22:04:09 +0900

    애플리케이션 실행 시 유저 하나 추가

--- src/main/java/me/jooomout/demorestapistudy/configs/AppConfig.java
+++ src/main/java/me/jooomout/demorestapistudy/configs/AppConfig.java
@@ -1,10 +1,18 @@
 package me.jooomout.demorestapistudy.configs;
 
+import me.jooomout.demorestapistudy.accounts.Account;
+import me.jooomout.demorestapistudy.accounts.AccountRole;
+import me.jooomout.demorestapistudy.accounts.AccountService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @Configuration
 public class AppConfig {
@@ -17,5 +25,24 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+
+            @Autowired
+            AccountService accountService;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Account jOut = Account.builder()
+                        .email("jOut@naver.com")
+                        .password("jjjjj")
+                        .roles(Set.of(AccountRole.USER, AccountRole.ADMIN))
+                        .build();
+                accountService.saveAccount(jOut);
+            }
+        };
     }
 }
