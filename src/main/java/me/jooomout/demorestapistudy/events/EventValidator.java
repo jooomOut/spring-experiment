d2e0f8bf75e0480bb1ccd4fa4f7c2c7b0a8553c0
jooomOut <rimeilo324@naver.com>
2021-10-13 16:03:58 +0900

    error 내용을 메시지로 관리

--- src/main/java/me/jooomout/demorestapistudy/events/EventValidator.java
+++ src/main/java/me/jooomout/demorestapistudy/events/EventValidator.java
@@ -1,5 +1,7 @@
 package me.jooomout.demorestapistudy.events;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
@@ -10,16 +12,22 @@ import java.time.LocalDateTime;
 
 @Component
 public class EventValidator {
+    @Autowired
+    MessageSource ms;
 
     public void validate(EventDto eventDto, BindingResult errors){
         if (eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() != 0) {
             // rejectValue - 필드에 들어가는 에러
             //errors.rejectValue("basePrice", "wrongValue", "BasePrice is wrong");
             // reject - 글로벌 에러
-            errors.addError(new ObjectError("event", "wrong value")); //글로벌 에러
+            //errors.addError(new ObjectError("event", "wrong value")); //글로벌 에러
             // 필드 에러
-            errors.addError(new FieldError("event", "price", "value for price are wrong"));
-            errors.reject("wrongPrices", "Values for prices are wrong");
+            System.out.println(ms.getMessage("wrong.event.price", null, null));
+
+            errors.addError(new FieldError("event", "price", null, false,
+                                    new String[]{"wrong.event.price"}, null,
+                    ms.getMessage("wrong.event.price", null, null)));
+            //errors.reject("wrongPrices", "Values for prices are wrong");
         }
 
         LocalDateTime closeEnrollmentDateTime = eventDto.getCloseEnrollmentDateTime();
